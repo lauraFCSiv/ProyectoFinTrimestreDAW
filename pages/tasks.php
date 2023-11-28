7e708959461da8ef2f34c485eecba3aa878e264d
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-11-28 13:25:15 +0100

    MACC#29

--- pages/tasks.php
+++ pages/tasks.php
@@ -1,2 +1,190 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LIBRERÍAS  -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="completedTask.css">
+    <title>Tareas completadas</title>
+</head>
+
+<body>
+    <!-- //! Cosas extra a tener en cuenta: dialog
+    //*Buscar cómo hacer funcional un pop up por cada pestaña
+    //* Que cada card responda ante el cursor si pasa por encima (hover) cambiando el tamaño -->
+    <!-- //*usar CSS para colores específicos -->
+
+    <div class="container">
+        <h1>Header</h1>
+
+        <!-- //*buscador  -->
+        <div class="row mt-5">
+            <div class="col">
+                <form method="POST">
+                    <div class="form-group">
+                        <input type="search" class="form-control border border-dark-subtle" id="" name="" placeholder="Buscar tareas">
+                    </div>
+                </form>
+            </div>
+        </div>
+
+            <!-- //*Cartas con tareas  -->
+        <div class="row mt-5 align-items-center">
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+                    
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría:
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo  + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //*Primera fila  -->
+
+            <!-- //*Cartas con tareas  -->
+        <div class="row mt-5 align-items-center">
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+                    
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría:
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo  + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+            <!-- //*Cartas con tareas  -->
+            <div class="row mt-5 align-items-center">
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+                    
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría:
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center border border-black">
+                    <div class="card-header text-dark bg-warning">
+                        <h5>Título de ejemplo  + fecha</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            Categoría
+                        </div>
+                        <button class="btn btn-primary mt-2 text-bg-warning btn-outline-dark">Detalles</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+            <button type="button" class="btn btn-warning btn-rounded mt-5" data-mdb-ripple-init>Crear nueva tarea</button>
+            </div>
+            <!-- //*=== -->
+        </div>
+    </div>
+</body>
+
+</html>
