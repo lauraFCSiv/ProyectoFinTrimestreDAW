8ecee0e42b06c352368ecf83dc829f79c2804907
pabloalbrdz <pabloalbrdz02@gmail.com>
2023-11-28 12:40:15 +0100

    PAR #6 LoginFuncionalidad

--- connection/connectionDataBase.php
+++ connection/connectionDataBase.php
@@ -1,2 +1,48 @@
 <?php
+/**
+ * @package ConnectionDatabase
+ */
+
+
+/**
+ * @version 1.0.
+ * @author Pablo A.
+ * @return mixed
+ * Metodo que abre la conexion con el servidor de base de datos.
+ */
+function openConnectionDB() {
+
+    // Parametros de la conexion.
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "";
+    $db = "taskmanager";
+
+    // Intento de conexion.
+    $conn = new mysqli($servername, $username, $password, $db);
+    
+    if ($conn->connect_error) {
+        // Fallo de conexion: lanzar excepcion.
+        die("Conexion fallada: " . $conn->connect_error +" \n");
+        throw new Exception("Conexion fallada");
+    }else {
+        // Conexion correcta: Devolver conexion activa.
+        return $conn;
+    }
+    
+}
+
+/**
+ * @version 1.0.
+ * @author Pablo A.
+ * Metodo que cierra la conexion con el servidor de base de datos.
+ */
+function closeConnectionDB($conn){
+   try{
+        $conn->close();
+   }catch(Exception $e){
+        $e->getMessage();
+   }
+}
+
 ?>
