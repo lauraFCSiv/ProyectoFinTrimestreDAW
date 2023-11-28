764869d6f51509ccdd159be1d23f8e01e0ffce2e
pabloalbrdz <pabloalbrdz02@gmail.com>
2023-11-28 14:28:29 +0100

    PAR #6 LoginEstilos1

--- pages/login.php
+++ pages/login.php
@@ -6,13 +6,15 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/StylesClaro.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
     <?php
+        include("../includes/header.php");
     ?>
     <main class="container">
-        <div class="row justify-content-center align-items-center">
-            <div class="col.12 text-center">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center justify-content-center">
                 <h2>Iniciar Sesion</h2>
                 <form method="POST">
                     <label>Usuario</label>
@@ -44,5 +46,8 @@
             </div>
         </div>
     </main>
+    <?php
+        include("../includes/footer.php");
+    ?>
 </body>
 </html>
