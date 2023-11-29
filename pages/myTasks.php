dd5d01e133c48b932d276fe5e4ead89d80c3d594
euse13 <eusebitouribarri@gmail.com>
2023-11-29 10:37:03 +0100

    Creacion de la barra

--- pages/myTasks.php
+++ pages/myTasks.php
@@ -1,2 +1,29 @@
 <?php
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Tabla Bootstrap</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container mt-4">
+    <form method="post" action="">
+        <div class="input-group mb-3">
+            <input type="search" class="form-control" name="search" placeholder="Buscar por nombre de tarea">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" name="submit">Buscar</button>
+            </div>
+        </div>
+    </form>
+
+    
+</div>
+    
+</body>
+</html>
