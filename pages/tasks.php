f4317707c05f6d6e41ac2733827a2ed42cd5c6e9
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-11-29 10:21:26 +0100

    MACC#29 incluyo link bootstrap para footer

--- pages/tasks.php
+++ pages/tasks.php
@@ -15,6 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="StyleClaro.css">
     <title>Tareas completadas</title>
 </head>
