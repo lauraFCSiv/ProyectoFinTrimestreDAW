05b9d208e487cac2b2131efaddc94f13af52f957
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-11-29 14:00:30 +0100

    MACC#29 se añade más cambios relaciondos con los estilos

--- pages/tasks.php
+++ pages/tasks.php
@@ -22,12 +22,10 @@
 
 <body>
 
-    <!-- //! Cosas extra a tener en cuenta: dialog
-    //*Buscar cómo hacer funcional un pop up por cada pestaña
+    <!-- //! Cosas extra a tener en cuenta:
     //* Que cada card responda ante el cursor si pasa por encima (hover) cambiando el tamaño -->
-    <!-- //*usar CSS para colores específicos -->
     <?php
-        include("../includes/header.php");
+    include("../includes/header.php");
     ?>
 
     <div class="container">
@@ -36,65 +34,95 @@
             <div class="col">
                 <form method="POST">
                     <div class="form-group">
-                        <input type="search" class="form-control border border-dark-subtle" id="" name="" placeholder="Buscar tareas">
+                        <input type="search" class="form-control border border-dark-subtle" id="" name=""
+                            placeholder="Buscar tareas">
                     </div>
                 </form>
             </div>
         </div>
 
-            <!-- //*Cartas con tareas  -->
+        <!-- //*Cartas con tareas  -->
         <div class="row mt-5 align-items-center">
             <div class="col">
-                <div class="card text-center border border-black">
+                <div class="card text-center border border-black" id="idCard1">
                     <div class="card-header text-dark">
-                        <h5>Título de ejemplo + fecha</h5>
+                        <h5>Título de ejemplo</h5>
                     </div>
-                    
+
                     <div class="card-body">
                         <div class="card-text">
-                            Categoría:
+                            <p>Categoría:</p>
+                            <p>Fecha</p>
                         </div>
-                        <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark">Detalles</button>
+                        <!-- <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal1">Detalles</button> -->
+                    </div>
+
+                </div>
+            </div>
+            <!-- //*Popup de la carta (Modal) -->
+            <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Título de la tarea</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Todo el contenido de la tarea
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <!-- <button type="button" class="btn btn-primary">Agregar tarea</button> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //*Fin del Popup de la carta -->
+                    <!-- //*Primera fila de cartas acaba aquí  -->
+            <!-- 2 cartas más solo para ver como queda, esto se borra luego  -->
+            <div class="col">
+                <div class="card text-center border border-black" id="idCard2">
+                    <div class="card-header text-dark">
+                        <h5>Título de ejemplo</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="card-text">
+                            <p>Categoría:</p>
+                            <p>Fecha</p>
+                        </div>
+                        <!-- <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal1">Detalles</button> -->
                     </div>
 
                 </div>
             </div>
             <div class="col">
-                <div class="card text-center border border-black">
+                <div class="card text-center border border-black" id="idCard3">
                     <div class="card-header text-dark">
-                        <h5>Título de ejemplo + fecha</h5>
+                        <h5>Título de ejemplo</h5>
                     </div>
 
                     <div class="card-body">
                         <div class="card-text">
-                            Categoría
+                            <p>Categoría:</p>
+                            <p>Fecha</p>
                         </div>
-                        <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark">Detalles</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card text-center border border-black">
-                    <div class="card-header text-dark">
-                        <h5>Título de ejemplo  + fecha</h5>
+                        <!-- <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal1">Detalles</button> -->
                     </div>
 
-                    <div class="card-body">
-                        <div class="card-text">
-                            Categoría
-                        </div>
-                        <button class="buttonCardsTasks btn btn-primary mt-2 btn-outline-dark">Detalles</button>
-                    </div>
                 </div>
             </div>
-        </div>
-        <!-- //*Primera fila de cartas acaba aquí  -->
-        
-        </div>
+        </div>        
+    </div>
     <?php
     include("../includes/footer.php");
     ?>
     </div>
+    <script src="../js/tasks.js"></script>
 </body>
 
 </html>
