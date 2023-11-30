dc2b14119a0cae7f5fdc1abca323d63d8a7c542a
alejandrus9 <alepenelas3@gmial.com>
2023-11-30 13:22:40 +0100

    ALP #7 inicio header y footer

--- pages/index.php
+++ pages/index.php
@@ -1,2 +1,23 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TurronTasker: Iniciar Sesion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../styles/StylesClaro.css">
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <?php
+        include("../includes/header.php");
+    ?>
+    <main class="mt-5">
+        
+    </main>
+    <?php
+        include("../includes/footer.php");
+    ?>
+</body>
+</html>
