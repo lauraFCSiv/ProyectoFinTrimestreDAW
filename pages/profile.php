158df158ff9fc60a59df095fee150bbbbe680bde
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-12-05 12:56:19 +0100

    MACC#20 - mínimos cambios de prueba

---
+++ pages/profile.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LIBRERÍAS  -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../styles/StylesClaro.css">
+    <title>Perfil de usuario</title>
+</head>
+
+<body>
+    <!-- header  -->
+    <?php
+    include("../includes/header.php");
+    ?>
+    
+    
+
+    <!-- footer  -->
+    <?php
+    include("../includes/footer.php");
+    ?>
+
+</body>
+
+</html>
