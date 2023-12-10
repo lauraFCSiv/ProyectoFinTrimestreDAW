54f6aa887097167bc8c6261ea5fd2b419ec878ae
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-12-10 18:31:42 +0100

    MACC#20 permitir al usuario cambiar estilos

--- js/tasks.js
+++ js/tasks.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Obtenemos el modal de x carta
-    var cards = document.querySelectorAll('.card');
+    let cards = document.querySelectorAll('.card');
     cards.forEach(function (card) {
         // Agregamos un evento click a cada carta
         card.addEventListener('click', function () {
             // Obtenemos el ID del modal 
-            var modalId = card.getAttribute('data-bs-target');
+            let modalId = card.getAttribute('data-bs-target');
             // Abrimos modal
-            var modal = new bootstrap.Modal(document.querySelector(modalId));
+            let modal = new bootstrap.Modal(document.querySelector(modalId));
             modal.show();
         });
     });
