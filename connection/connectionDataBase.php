680b5bbd7e28f4e17f1cca613fb39665c70c6631
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2023-12-12 12:17:39 +0100

    contraseña root

--- connection/connectionDataBase.php
+++ connection/connectionDataBase.php
@@ -3,7 +3,6 @@
  * @package ConnectionDatabase
  */
 
-
 /**
  * @version 1.0.
  * @author Pablo A.
@@ -11,25 +10,24 @@
  * Metodo que abre la conexion con el servidor de base de datos.
  */
 function openConnectionDB() {
-
     // Parametros de la conexion.
     $servername = "localhost:3306";
     $username = "root";
+    $password = "Eusebiouribarri13";
     $password = "";
+
     $db = "taskmanager";
 
     // Intento de conexion.
     $conn = new mysqli($servername, $username, $password, $db);
-    
+
     if ($conn->connect_error) {
         // Fallo de conexion: lanzar excepcion.
-        die("Conexion fallada: " . $conn->connect_error +" \n");
-        throw new Exception("Conexion fallada");
-    }else {
+        die("Conexion fallada: " . $conn->connect_error . " \n");
+    } else {
         // Conexion correcta: Devolver conexion activa.
         return $conn;
     }
-    
 }
 
 /**
@@ -38,11 +36,12 @@ function openConnectionDB() {
  * Metodo que cierra la conexion con el servidor de base de datos.
  */
 function closeConnectionDB($conn){
-   try{
+    try {
         $conn->close();
-   }catch(Exception $e){
-        $e->getMessage();
-   }
+    } catch (Exception $e) {
+        // Puedes loggear o imprimir el mensaje de la excepción.
+        echo "Error al cerrar la conexión: " . $e->getMessage();
+    }
 }
 
 ?>
