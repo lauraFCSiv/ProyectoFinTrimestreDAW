c428b6ff4b01ce491358f8ba3b4eacf22bcf6d78
euse13 <eusebitouribarri@gmail.com>
2023-12-14 12:06:18 +0100

    signup

--- includes/header.php
+++ includes/header.php
@@ -87,23 +87,31 @@ session_start();
                                     ';
                         } else {
                             echo '
-                                        <li class="nav-item px-lg-5 dropdown">
-                                            <button class="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                                                </svg>
-                                                Usuario
-                                            </button>
-                                            <ul class="dropdown-menu">
-                                                <li>
-                                                    <a class="dropdown-item" type="button" href="login.php">
-                                                        Iniciar Sesion                                                   
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    ';
+                            <li class="nav-item px-lg-5 dropdown">
+                                <button class="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                                    </svg>
+                                    Usuario
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" type="button" href="login.php">
+                                            Iniciar Sesión
+                                        </a>
+                                    </li>
                                     
+                                    <!-- Nueva opción para registrarse -->
+                                    <li>
+                                        <a class="dropdown-item" type="button" href="signup.php">
+                                            Registrarse
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        ';
+
+                                      
                         }
                         ?>
                         <!-- Nuevo Dropdown -->
